8976034c300800a1d690acda93e43b92495bb47f
Alberto Colom Monfort <acolom@encamina.com>
2022-02-13 08:34:10 +0100

    Added more tests

--- src/lib/lib.tests/MaybeTests.cs
+++ src/lib/lib.tests/MaybeTests.cs
@@ -6,7 +6,6 @@ namespace lib.tests
     public class MaybeTests
     {
         [Fact]
-
         public void If_Access_Value_On_Fail_Expect_Exception()
         {
             var failed = Maybe<int>.None();
@@ -14,7 +13,6 @@ namespace lib.tests
         }
 
         [Fact]
-
         public void CheckIdentity()
         {
             Maybe<int> maybeInt = 1;
@@ -24,7 +22,6 @@ namespace lib.tests
         }
 
         [Fact]
-
         public void CheckIdentity_Ok()
         {
             Maybe<int> maybeInt = Maybe<int>.Ok(1);
@@ -33,5 +30,99 @@ namespace lib.tests
             Assert.True(maybeInt.Value == 1);
         }
 
+        [Fact]
+        public void Bind_Test_Execution()
+        {
+            Maybe<int> maybeInt = Maybe<int>.Ok(1);
+            var returnSuccess = (int a) => Maybe<int>.Ok(a);
+
+            var result = maybeInt
+                .Bind((a) => returnSuccess(a))
+                .Bind(returnSuccess);
+
+            Assert.True(result.Success);
+        }
+
+        [Fact]
+        public void Match_Bind_Execution_Success()
+        {
+            Maybe<int> maybeInt = Maybe<int>.Ok(1);
+            Func<int, Maybe<int>> returnFailure = (int a) => Maybe<int>.None();
+            var returnSuccess = (int a) => Maybe<int>.Ok(a);
+            var executed = false;
+            var markExecuted = (int a) =>
+            {
+                executed = true;
+                return Maybe<int>.Ok(a);
+            };
+
+            maybeInt
+                .Bind((a) => returnSuccess(a))
+                .Bind(returnSuccess)
+                .Bind(markExecuted);
+
+            Assert.True(executed);
+        }
+
+        [Fact]
+        public void Match_Bind_Execution_Failure()
+        {
+            Maybe<int> maybeInt = Maybe<int>.Ok(1);
+            Func<int, Maybe<int>> returnFailure = (int a) => Maybe<int>.None();
+            var returnSuccess = (int a) => Maybe<int>.Ok(a);
+            var executed = false;
+            var markExecuted = (int a) =>
+            {
+                executed = true;
+                return Maybe<int>.Ok(a);
+            };
+
+            maybeInt
+                .Bind((a) => returnSuccess(a))
+                .Bind(returnFailure)
+                .Bind(markExecuted);
+
+            Assert.False(executed);
+        }
+
+
+        [Fact]
+        public void Match_Test_Execution_Success()
+        {
+            Maybe<int> maybeInt = Maybe<int>.Ok(1);
+            var returnSuccess = (int a) => Maybe<int>.Ok(a);
+            var executed = false;
+            var markExecuted = (int a) =>
+            {
+                executed = true;
+            };
+
+            maybeInt
+                .Bind((a) => returnSuccess(a))
+                .Bind(returnSuccess)
+                .Match(markExecuted);
+
+            Assert.True(executed);
+        }
+
+        [Fact]
+        public void Match_Test_Execution_Failure()
+        {
+            Maybe<int> maybeInt = Maybe<int>.Ok(1);
+            var returnSuccess = (int a) => Maybe<int>.Ok(a);
+            var returnFailure = (int a) => Maybe<int>.None();
+            var executed = false;
+            var markExecuted = (int a) =>
+            {
+                executed = true;
+            };
+
+            maybeInt
+                .Bind((a) => returnSuccess(a))
+                .Bind(returnFailure)
+                .Match(markExecuted);
+
+            Assert.False(executed);
+        }
     }
 }
