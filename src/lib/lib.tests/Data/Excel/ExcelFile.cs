2acefcc6d7c24c60c813eff77d51191fe234ce63
Alberto Colom Monfort <acolom@encamina.com>
2022-02-13 07:49:22 +0100

    Add tests and sample

---
+++ src/lib/lib.tests/Data/Excel/ExcelFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib.tests.Data.Excel
+{
+
+    public class File
+    {
+        public static Maybe<File> CreateFromPath(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                return Maybe<File>.None();
+
+            return new File();
+        }
+    }
+
+    public class ExcelFile
+    {
+        public static Maybe<ExcelFile> CreateFromFile(File file)
+        {
+            return new ExcelFile();
+        }
+    }
+
+    public class ExcelSheet
+    {
+        public static Maybe<ExcelSheet> Find(ExcelFile file, string sheetName)
+        {
+            if (string.IsNullOrWhiteSpace(sheetName))
+                return Maybe<ExcelSheet>.None();
+
+            return new ExcelSheet();
+        }
+    }
+
+    public class ExcelCell
+    {
+        public static Maybe<ExcelCell> FindInSheet(ExcelSheet file, string cellReference)
+        {
+            if (string.IsNullOrWhiteSpace(cellReference))
+                return Maybe<ExcelCell>.None();
+
+            return new ExcelCell();
+        }
+    }
+}
---
+++ src/lib/lib.tests/Step01/Step01.cs
@@ -0,0 +1,117 @@
+﻿using lib.tests.Data.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace lib.tests.Step01
+{
+    public class Step01
+    {
+        [Fact]
+
+        public void Step01_If_Access_Value_On_Fail_Expect_Exception()
+        {
+            var failed = Maybe<int>.None();
+            Assert.Throws<InvalidOperationException>(() => failed.Value);
+        }
+
+        [Fact]
+
+        public void Step01_CheckIdentity()
+        {
+            Maybe<int> maybeInt = 1;
+
+            Assert.True(maybeInt.Success);
+            Assert.True(maybeInt.Value == 1);
+        }
+
+        [Fact]
+
+        public void Step01_CheckIdentity_Ok()
+        {
+            Maybe<int> maybeInt = Maybe<int>.Ok(1);
+
+            Assert.True(maybeInt.Success);
+            Assert.True(maybeInt.Value == 1);
+        }
+
+
+        public void Step01_Sample_01()
+        {
+            // Abrir fichero
+            // Recoger excel
+            // encontrar el sheet 
+            // recogert el cellvalue
+            var path = string.Empty;
+            var sheetName = string.Empty;
+            var cellReference = string.Empty;
+
+            var file = File.CreateFromPath(path);
+            if (file.Success)
+            {
+                var excelFile = ExcelFile.CreateFromFile(file.Value);
+                if (excelFile.Success)
+                {
+                    var sheet = ExcelSheet.Find(excelFile.Value, sheetName);
+                    if (sheet.Success)
+                    {
+                        var cell = ExcelCell.FindInSheet(sheet.Value, cellReference);
+                        if (cell.Success)
+                        {
+                            // hacer algo con cell value
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        public void Step01_Sample_02()
+        {
+            // Abrir fichero
+            // Recoger excel
+            // encontrar el sheet 
+            // recogert el cellvalue
+
+            var path = string.Empty;
+            var sheetName = string.Empty;
+            var cellReference = string.Empty;
+
+            var file = File.CreateFromPath(path);
+            if (!file.Success)
+                return;
+
+            var excelFile = ExcelFile.CreateFromFile(file.Value);
+            if (!excelFile.Success)
+                return;
+
+            var sheet = ExcelSheet.Find(excelFile.Value, sheetName);
+            if (!sheet.Success)
+                return;
+
+            var cell = ExcelCell.FindInSheet(sheet.Value, cellReference);
+            if (!cell.Success)
+                return;
+
+            // hacer algo con cell value
+        }
+    }
+}
